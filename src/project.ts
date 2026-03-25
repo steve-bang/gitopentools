96473dc748a9cc543d412d1733c0b339b6bf988d
Steve Bang <mrsteve.bang@gmail.com>
2026-03-25 22:16:48 +0700

    chore(project): update copyright year

--- src/project.ts
+++ src/project.ts
@@ -3,7 +3,7 @@ export const PROJECT_INFO = {
   name: "GitOpen Tools",
   description: "45+ free developer tools to boost your productivity and streamline your workflow.",
   logo: "/assets/logo_trans.png",
-  copyright: "© 2025 GitOpen Tools. All rights reserved.",
+  copyright: "© 2026 GitOpen Tools. All rights reserved.",
   domain: "gitopentools.com",
   socials: {
     github: "https://github.com/steve-bang/gitopentools",
